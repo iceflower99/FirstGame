e1c327cc87f315642eb50c9d31df817b6d8e23bf
iceflower99 <mhal4767@740s-pl-120.local>
2016-05-12 08:55:36 -0600

    worked on stuff

---
+++ FirstGame/Veiw/Animation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+namespace FirstGame
+{
+	public class Animation
+	{
+		public Animation ()
+		{
+		}
+	}
+}
+
